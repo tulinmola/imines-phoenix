be520814e90517b5bffe1332196a1bc900f5ff7d
tulinmola <tulinmola@gmail.com>
2016-05-07 12:38:29 +0200

    image base 64 inline encoder

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule Imines.LayoutView do
   use Imines.Web, :view
+
+  def encoded_image(path, attrs \\ []) do
+    {:ok, body} = File.read("web/static/assets/images/#{path}")
+    src = "data:image/png;base64,#{Base.encode64(body)}"
+    attrs = [src: src] ++ attrs
+    tag(:img, attrs)
+  end
 end
